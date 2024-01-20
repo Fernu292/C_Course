07deb926ac578e8abbbafff80ca2ec04c69707df
Fernando <fernu292@gmail.com>
2024-01-19 22:35:45 -0600

    Makefile and Linked multiple files

---
+++ FirstProject/Other.c
@@ -0,0 +1,7 @@
+#include "./Headers/Other.h"
+
+int getFavoriteNumber(void){
+
+    return 3;
+
+}
--- FirstProject/main.c
+++ FirstProject/main.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
+#include "./Headers/Other.h"
+#include "./Headers/date.h"
 
 int main(int argc, char **argv){
-    
-    int favNumber = 0;
-    printf("\n\t Please enter your favorite number: ");
-    scanf("%d", &favNumber);
-    
-    printf("\n\t That's my favorite number too, %d \n\n", favNumber);
+      
+      int favNumber = 0;
+      printf("\n\t Please enter your favorite number: ");
+      scanf("%d", &favNumber);
+      
+      printf("\n\t That's my favorite number too, %d \n\n", favNumber);
+  
+      printf("\n\t Favorite number: %d \n\n", getFavoriteNumber() );
 
-    return 0;
+      return 0;
+  
 }
